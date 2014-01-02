393a4e671437fedba777bcea6584f8f976f1d6c3
Benjamin Shyong <ben.shyong@gmail.com>
2014-01-02 02:15:14 -0800

    specify class for outfit/tie association

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -2,5 +2,5 @@ class Outfit < ActiveRecord::Base
   has_and_belongs_to_many :shirts
   has_and_belongs_to_many :pants
   has_and_belongs_to_many :jackets
-  has_and_belongs_to_many :ties
+  has_and_belongs_to_many :ties, class_name: 'Tie'
 end
